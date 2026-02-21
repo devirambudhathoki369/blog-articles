954fa6681ee140e45672a81c6c51d847555767ce
devirambudhathoki369 <devirambudhathoki369@gmail.com>
2026-02-21 19:59:23 +0545

    Category model modified

--- blogs/models.py
+++ blogs/models.py
@@ -5,3 +5,9 @@ class Category(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.category_name
+
