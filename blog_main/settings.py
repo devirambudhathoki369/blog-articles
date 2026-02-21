ff88f27e922a59c206a69a4557b8e965a5351516
devirambudhathoki369 <devirambudhathoki369@gmail.com>
2026-02-21 17:37:19 +0545

    blog category model added

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blogs',
 ]
 
 MIDDLEWARE = [
@@ -114,9 +115,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-STATIC_URL = BASE_DIR / 'static'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
