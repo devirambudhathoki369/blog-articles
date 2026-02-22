48ba1b8708a07fbe3e6f8ab58a743184e5ef296a
devirambudhathoki369 <devirambudhathoki369@gmail.com>
2026-02-22 06:22:25 +0545

    Blog model added

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
-from .models import Category
+from .models import Category, Blog
 
+class BlogAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+    list_display = ('title', 'category', 'author', 'status',)
+    search_fields = ('title', 'category__category_name', 'author__username', 'status',)
+    list_editable = ('status',)
 admin.site.register(Category)
+admin.site.register(Blog, BlogAdmin)
