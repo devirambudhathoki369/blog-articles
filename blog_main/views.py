90971e84f4277519251e808e3d0e925f08d148f9
devirambudhathoki369 <devirambudhathoki369@gmail.com>
2026-02-22 07:32:17 +0545

    Post by category added

--- blog_main/views.py
+++ blog_main/views.py
@@ -4,8 +4,10 @@ from blogs.models import Blog, Category
 def home(request):
     categories = Category.objects.all()
     featured_posts = Blog.objects.filter(featured_post=True, status=1).order_by('-created_at')
+    posts = Blog.objects.filter(status=1, featured_post=False).order_by('-created_at')
     context = {
         'categories': categories,
-        'featured_posts': featured_posts
+        'featured_posts': featured_posts,
+        'posts': posts
     }
     return render(request, 'home.html', context)
--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,13 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Blog, Category
 
-# Create your views here.
+
+def post_by_category(request, category_id):
+    posts = Blog.objects.filter(category=category_id, status=1)
+    category = Category.objects.get(pk=category_id)
+    context = {
+        'posts': posts,
+        'category': category
+    }
+    return render(request, 'posts_by_category.html', context)
