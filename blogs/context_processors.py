ab557f16653491006e7869e6d5284b58e03f1c1a
devirambudhathoki369 <devirambudhathoki369@gmail.com>
2026-04-02 05:11:16 +0545

    home page modified

--- blogs/context_processors.py
+++ blogs/context_processors.py
@@ -1,6 +1,10 @@
-from blogs.models import Category
+from blogs.models import Category, SocialMedia
 
 
 def get_categories(request):
     categories = Category.objects.all()
     return dict(categories=categories)
+
+def get_social_media_links(request):
+    social_media_links = SocialMedia.objects.all()
+    return dict(social_media_links=social_media_links)
